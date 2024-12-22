1a8a2d35a1e05610e5b84156a307e0276492bfb7
kemthegamedev <kemal@simulakra.de>
2024-12-22 17:21:06 +0300

    PlayerController placeholder

---
+++ Source/HeroScrapyard/Public/HS_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HS_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROSCRAPYARD_API AHS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
