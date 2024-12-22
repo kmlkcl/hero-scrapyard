fe4005c252c140a3f7a7ab1822d5d09cb474d049
kemthegamedev <kemal@simulakra.de>
2024-12-22 16:35:32 +0300

    placeholder gamemode

---
+++ Source/HeroScrapyard/Public/HS_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HS_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROSCRAPYARD_API AHS_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
